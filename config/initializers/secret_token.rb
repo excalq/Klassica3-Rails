a81b6e9770aa49683acc5f1e370f0335d9f591c2
Arthur Ketcham <arthur@kaskadia.com>
2011-10-25 15:28:21 -0600

    Created Klassica3-rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klassica3::Application.config.secret_token = '0bdb88263d50ccf4e98e84062db7401e77577b9b0d91d771c934dabf830d081588e63cc36294a5b30424f37544a67f20ed60f9b20e7b3128ea10c6c6acbe90a4'
